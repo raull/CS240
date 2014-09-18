4243fd5fd2ac9871f1ca998a34506ddab508296c
Raul Lopez <ruffles91@gmail.com>
2014-09-18 09:32:51 -0600

    Created All the classes

--- Project_3/src/listem/FileSearch.java
+++ Project_3/src/listem/FileSearch.java
@@ -1,5 +1,11 @@
 package listem;
 
+import java.io.File;
+import java.util.List;
+
 public class FileSearch {
 	
+	public List<File> search(File directory, String fileSelectionPattern, boolean recursive) {
+		return null;
+	}
 }
